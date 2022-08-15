79be0abb4fe3aa53739c63ad1b67dadf980e5688
Yannan Cai <96087981+yannancai@users.noreply.github.com>
2022-08-15 01:14:20 -0400

    Update solution.py

--- solution.py
+++ solution.py
@@ -1,136 +1,34 @@
-from abc import ABC, abstractmethod
-from collections import deque
+class LockerBox:
+    def __init__(self, size_list):
+        self.__size_list = size_list.sort()
+        self.__is_empty = [True] * len(self.__size_list)
+        self.__open_id = 0
+        self.__dict_openid_index = {}
 
+    def find_empty(self, item_size):
+        if item_size > self.__size_list[-1]:
+            return None
 
-class Node:
-    def __init__(self, name, is_directory: bool, extension, size: int):
-        self.name = name
-        self.is_directory = is_directory
-        self.extension = extension
-        self.size = size
-        self.children = []
+        start_index = self.__binary_search(item_size)
+        for index in range(start_index, len(self.__size_list)):
+            if not self.__is_empty[index]:
+                continue
+            self.__open_id += 1
+            self.__dict_openid_index[self.__open_id] = index
+            self.__is_empty[index] = False
+            return index, self.__open_id    # __open_id is used as item id
 
-    def set_children(self, node):
-        self.children.append(node)
+        return None
 
+    def find_item(self, open_id):
+        if open_id not in self.__dict_openid_index:
+            return None
+        box_index = self.__dict_openid_index[open_id]
+        self.__dict_openid_index.pop(open_id)
+        self.__is_empty[box_index] = True
+        return box_index
 
-class LinuxSearch:
-    def search(self, criteria, node):
-        if not node.is_directory:
-            raise Exception("Node is not directory")
-
-        search_result = []
-        queue = deque()
-
-        for child in node.children:
-            queue.append(child)
-
-        while queue:
-            cur_node = queue.popleft()
-            if criteria.is_valid(cur_node):
-                search_result.append(cur_node)
-            if cur_node.children:
-                for child in cur_node.children:
-                    queue.append(child)
-
-        return search_result
-
-
-class Criteria(ABC):
-    @abstractmethod
-    def is_valid(self, node):
-        pass
-
-
-class ByExtension(Criteria):
-    def __init__(self, extension):
-        self.extension = extension
-
-    def is_valid(self, node):
-        if node.extension is self.extension:
-            return True
-
-
-class ByNodeSize(Criteria, ABC):
-    @abstractmethod
-    def __init__(self, size):
-        self.size = size
-
-    @abstractmethod
-    def is_valid(self, node):
-        pass
-
-
-class ByGreatOrEqual(ByNodeSize):
-    def __init__(self, size):
-        super().__init__(size)
-
-    def is_valid(self, node):
-        if node.size >= self.size:
-            return True
-
-
-class AndCriteria(Criteria):
-    def __init__(self, criteria_a, criteria_b):
-        self.criteria_a = criteria_a
-        self.criteria_b = criteria_b
-
-    def is_valid(self, node):
-        return self.criteria_a.is_valid(node) and self.criteria_b.is_valid(node)
-
-
-class OrCriteria(Criteria):
-    def __init__(self, criteria_a, criteria_b):
-        self.criteria_a = criteria_a
-        self.criteria_b = criteria_b
-
-    def is_valid(self, node):
-        return self.criteria_a.is_valid(node) or self.criteria_b.is_valid(node)
-
-
-home = Node("home", True, None, 100)
-movie = Node("movie", True, None, 70)
-music = Node("music", True, None, 20)
-resume = Node("resume", False, "txt", 10)
-alpha = Node("alpha", False, "txt", 15)
-beta = Node("beta", False, "mp4", 60)
-gamma = Node("gamma", False, "mp4", 15)
-cuban = Node("cuban", True, None, 5)
-
-home.set_children(movie)
-home.set_children(music)
-home.set_children(resume)
-movie.set_children(alpha)
-movie.set_children(beta)
-music.set_children(gamma)
-music.set_children(cuban)
-
-my_linux = LinuxSearch()
-
-is_txt = ByExtension("txt")
-search_home_txt = my_linux.search(is_txt, home)
-print("search_home_txt")
-print([(node.name, node.size) for node in search_home_txt])
-
-is_size_ge_15 = ByGreatOrEqual(15)
-search_movie_ge_15 = my_linux.search(is_size_ge_15, movie)
-print("search_movie_ge_15")
-print([(node.name, node.size) for node in search_movie_ge_15])
-
-is_txt_ge_15 = AndCriteria(is_txt, is_size_ge_15)
-search_home_txt_ge_15 = my_linux.search(is_txt_ge_15, home)
-print("search_home_txt_ge_15")
-print([(node.name, node.size) for node in search_home_txt_ge_15])
-
-is_mp4 = ByExtension("mp4")
-search_home_mp4 = my_linux.search(is_mp4, home)
-print("search_home_mp4")
-print([(node.name, node.size) for node in search_home_mp4])
-
-# txt ge 15 or mp4
-is_home_comp = OrCriteria(is_txt_ge_15, is_mp4)
-search_home_comp = my_linux.search(is_home_comp, home)
-print("search_home_comp")
-print([(node.name, node.size) for node in search_home_comp])
-
-search_gamma = my_linux.search(is_txt, gamma)
+    def __binary_search(self, item_size):
+        # a binary search to find the smallest box size which is equal to or larger than item_size
+        index = 0
+        return index
